ac33caaa6951df9e96f7a7267956a098167eb368
Developer Kabir <hakabir40@gmail.com>
2022-05-16 08:34:29 +0600

    data load from server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -9,6 +11,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_PASSWORD}@cluster0.hkeao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try{
+        await client.connect();
+        const carCollection = client.db('warehouse').collection('car');
+
+        app.get('/car' , async(req, res) => {
+            const query = {};
+            const cursor = carCollection.find(query);
+            const cars = await cursor.toArray();
+            res.send(cars);
+        });
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Warehouse server Running')
