609b4cddd0173d2f57387e51b3cdb5b4252e0ea9
Developer Kabir <hakabir40@gmail.com>
2022-05-20 03:15:02 +0600

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -12,7 +13,23 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next){
+    const authHeader = req.header.authorization;
+    if (!authHeader) {
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({message: 'forbidden access'})
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
 
+    })
+   
+}
 
 
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-shard-00-00.hkeao.mongodb.net:27017,cluster0-shard-00-01.hkeao.mongodb.net:27017,cluster0-shard-00-02.hkeao.mongodb.net:27017/?ssl=true&replicaSet=atlas-7i6kd9-shard-0&authSource=admin&retryWrites=true&w=majority`;
@@ -25,7 +42,14 @@ async function run() {
         const carCollection = client.db('warehouse').collection('car');
         const addCarCollection = client.db('warehouse').collection('addCar');
         
-        
+         //AUTH
+         app.post('/login' , async(req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.TOKEN, {
+                expiresIn: '1d'
+            });
+            res.send({accessToken});
+        })
 
         // get
         app.get('/car', async (req, res) => {
@@ -71,9 +95,11 @@ async function run() {
         app.get('/myitem', verifyJWT, async(req, res) => {
             const decodedEmail = req.decoded.email;
             const email = req.query.email;
+            console.log(email);
             console.log(decodedEmail);
             if (email === decodedEmail) {
             const query = {email: email};
+            console.log(query);
             const cursor = carCollection.find(query);
             const myitems = await cursor.toArray() ;
             res.send(myitems);
